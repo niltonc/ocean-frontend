d53e38de65147e0879dcc40b4153f5cb2b279cc0
niltonc <niltoncesarjr1@gmail.com>
2022-04-21 13:25:26 -0300

    feat(theme): add a theme config

---
+++ src/styles/theme/styled.d.ts
@@ -0,0 +1,31 @@
+import 'styled-components';
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    title: string;
+    colors: any;
+    fonts: any;
+
+    colors: {
+      primary: string,
+      secundary: string,
+      yellow: string,
+      black: string,
+      blue: string,
+      greylight: string,
+      grey: string,
+    };
+
+    fonts:{
+      header: string,
+      title: string,
+      button: string,
+      titlecard: string,
+      h1: string,
+      h2: string,
+      h3: string,
+      h4: string,
+    };
+
+  }
+}
---
+++ src/styles/theme/theme.tsx
@@ -0,0 +1,24 @@
+export default {
+    title: 'ocean-theme',
+  
+    colors: {
+      primary: '#00B9FF',
+      secundary: '#F5FCFF',
+      yellow: '#FFEBA3',
+      black: '#464646',
+      blue:'#004098',
+      greylight:'#F8F8F8',
+      grey:'#ECECEC',
+    },
+    
+    fonts:{
+      header:'10.64px',
+      title: '30.68px',
+      button: '17.25px',
+      titlecard: '15px',
+      h1: '11.35px',
+      h2: '11.4px',
+      h3: '9.17px',
+      h4: '7.3px',
+    },
+  };
