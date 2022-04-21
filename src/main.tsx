d30e37ff68aa52977adf0fc5a2fcf76c294dcd15
niltonc <niltoncesarjr1@gmail.com>
2022-04-21 13:26:45 -0300

    refactor(main): add theme provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ThemeProvider } from 'styled-components';
 import App from './router';
+import OceanTheme from './styles/theme/theme';
 
 ReactDOM.render(
+    <ThemeProvider theme={OceanTheme}>
     <React.StrictMode>
         <App />
-    </React.StrictMode>,
+    </React.StrictMode>
+    </ThemeProvider>,
     document.getElementById('root')
 );
