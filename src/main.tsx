d12e87a351bc88e0cfb06d304120363ec3e698e9
niltonc <niltoncesarjr1@gmail.com>
2022-04-22 12:33:17 -0300

    refactor(globalstyle): change position global style

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { ThemeProvider } from 'styled-components';
 import App from './router';
+import GlobalStyle from './styles';
 import OceanTheme from './styles/theme/theme';
 
 ReactDOM.render(
     <ThemeProvider theme={OceanTheme}>
+        <GlobalStyle/>
     <React.StrictMode>
         <App />
     </React.StrictMode>
