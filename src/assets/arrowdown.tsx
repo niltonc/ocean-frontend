5e89c3faf41bbaae0c301aecaeb107154c6b78b3
niltonc <niltoncesarjr1@gmail.com>
2022-04-23 18:19:14 -0300

    style(assets): add react

--- src/assets/arrowdown.tsx
+++ src/assets/arrowdown.tsx
@@ -1,3 +1,4 @@
+import * as React from 'react';
 import { SVGProps } from 'react';
 
 const ArrowDown = (props: SVGProps<SVGSVGElement>) => (
@@ -11,7 +12,7 @@ const ArrowDown = (props: SVGProps<SVGSVGElement>) => (
     <path
       fillRule="evenodd"
       clipRule="evenodd"
-      d="M8.692 4.817a.625.625 0 0 1-.884 0L4.5 1.509 1.192 4.817a.625.625 0 0 1-.884-.884l3.75-3.75a.625.625 0 0 1 .884 0l3.75 3.75a.625.625 0 0 1 0 .884Z"
+      d="M.308.183a.625.625 0 0 1 .884 0L4.5 3.491 7.808.183a.625.625 0 0 1 .884.884l-3.75 3.75a.625.625 0 0 1-.884 0l-3.75-3.75a.625.625 0 0 1 0-.884Z"
       fill="#464646"
     />
   </svg>
