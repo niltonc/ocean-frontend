726d528a80a5edd67d14fb2fb40d4875f38f48e3
niltonc <niltoncesarjr1@gmail.com>
2022-04-23 13:00:25 -0300

    style(header): eslint indentation

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,24 +1,23 @@
 import { Link } from 'react-router-dom';
-import * as H from './styles'
-import Logo from '../../assets/logo'
+import * as H from './styles';
+import Logo from '../../assets/logo';
 
-function Header (){
+function Header() {
+  return (
+    <H.Nav>
+      <H.Svg>
+        <Logo />
+      </H.Svg>
 
-    return(
-        <H.Nav>
-          <H.Svg>
-            <Logo />
-          </H.Svg>
+      <H.Links>
+        <Link to="/trilhas">Tilhas</Link>
+        <Link to="/">Cursos</Link>
+        <Link to="/agenda">Agenda</Link>
+      </H.Links>
+      <Link to="/">Entrar/Cadastrar</Link>
 
-            <H.Links>
-            <Link to="/trilhas">Tilhas</Link>
-            <Link to="/">Cursos</Link>
-            <Link to="/agenda">Agenda</Link>
-            </H.Links>
-            <Link to='/'>Entrar/Cadastrar</Link>
-
-        </H.Nav>
-    )
+    </H.Nav>
+  );
 }
 
 export default Header;
--- src/components/header/styles.tsx
+++ src/components/header/styles.tsx
@@ -3,25 +3,25 @@ import styled from 'styled-components';
 export const Nav = styled.header`
     width: 100%;
     height: 60px;
-    background: ${({theme})=> theme.colors.white};
+    background: ${({ theme }) => theme.colors.white};
     box-shadow: rgba(0, 0, 0, 0.15) 0px 5px 15px;
     display: flex;
     justify-content: space-around;
     align-items: center;
-   
+
     a {
-        color: ${({theme})=> theme.colors.blue};
+        color: ${({ theme }) => theme.colors.blue};
         font-weight: 500;
         text-decoration: none;
     }
-`
+`;
 export const Svg = styled.div`
     height: 25px;
-` 
+`;
 export const Links = styled.div`
     width: 30%;
     height: 60px;
     display: flex;
     align-items: center;
     justify-content: space-around;
-`
+`;
