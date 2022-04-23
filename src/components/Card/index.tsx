b30a98a2bbefd0f84fd7a566ec6799a7268121f2
niltonc <niltoncesarjr1@gmail.com>
2022-04-23 18:19:38 -0300

    refctor(card): add accordion

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -7,6 +7,9 @@ import IconHeaderCard from '../../assets/headercard';
 import IconHour from '../../assets/hour';
 import IconLocation from '../../assets/location';
 import IconUser from '../../assets/user';
+import IconArrowUp from '../../assets/arrowup';
+import IconArrowDown from '../../assets/arrowdown';
+import IconDegree from '../../assets/degree';
 
 function Card() {
   const urlAPI = 'https://gist.githubusercontent.com/creathusjobs/3c53322e8dc8c3b524cfb2623c097cc5/raw/adc988f34c4fb1a0fe12035b4b030259776115ea/response.json';
@@ -16,84 +19,152 @@ function Card() {
     axios.get(urlAPI).then((resp) => setCoursersDB(resp.data));
   }, []);
 
+  const [open, setOpen] = useState(false);
+  const handleClick = () => {
+    setOpen(!open);
+  };
+
   return (
     <div>
       <C.CardContainer>
         {coursersDB.map((courser) => (
-          <C.Container key={courser.track}>
-            <C.Color>
-              <C.SvgDiv>
-                <IconHeaderCard />
-              </C.SvgDiv>
+          <C.AccordionWrapper>
+            <C.Container key={courser.track}>
+              <C.Color>
+                <C.SvgDiv>
+                  <IconHeaderCard />
+                </C.SvgDiv>
 
-              <C.Title>
-                {courser.title}
-              </C.Title>
+                <C.Title>
+                  {courser.title}
+                </C.Title>
 
-              <C.Information>
-                <C.Row>
+                <C.Information>
+                  <C.Row>
+                    <IconCalendar />
+                    <C.Col>
+                      <C.H3>INÍCIO EM</C.H3>
+                      <C.H1>
+                        {courser.startTime}
+                      </C.H1>
+                    </C.Col>
+
+                    <IconCalendar />
+                    <C.Col>
+                      <C.H3>TÉRMINO</C.H3>
+                      <C.H1>
+                        {courser.endDate}
+                      </C.H1>
+                    </C.Col>
+                  </C.Row>
+
+                  <C.Row>
+                    <IconBook />
+                    <C.Col>
+                      <C.H3>3 AULAS</C.H3>
+                    </C.Col>
+
+                    <IconHour />
+                    <C.Col>
+                      <C.H3>12H TOTAL</C.H3>
+                    </C.Col>
+                  </C.Row>
+
+                  <C.Row>
+                    <IconUser />
+                    <C.Col>
+                      <C.H3>
+                        {courser.speakers}
+                      </C.H3>
+                    </C.Col>
+
+                    <IconLocation />
+                    <C.Col>
+                      <C.H3>
+                        {courser.campus}
+                      </C.H3>
+                    </C.Col>
+                  </C.Row>
+                </C.Information>
+
+                <C.DateTag>
+                  <C.TextTag>
+                    {courser.enrollmentsStart}
+                  </C.TextTag>
+                </C.DateTag>
+
+                <C.Button>
+                  Página do Curso
+                </C.Button>
+              </C.Color>
+            </C.Container>
+
+            <C.RowWrapper>
+              <C.ColWrapper>
+                <C.TextWrapper>
+                  Sobre as aulas
+                </C.TextWrapper>
+              </C.ColWrapper>
+              <C.ButtonWrapper onClick={handleClick}>
+                <IconArrowDown />
+              </C.ButtonWrapper>
+            </C.RowWrapper>
+
+            <C.InternalWrapper open={open}>
+
+              {/* 1 */}
+              <C.RowAccordion>
+                <IconBook />
+                <C.H3>4 DE FEVEREIRO</C.H3>
+                <C.H3>18:30 - 22:30</C.H3>
+              </C.RowAccordion>
+
+              <C.DivAccordion>
+                <C.TextAccordion>Parte 1</C.TextAccordion>
+                <C.SvgAccordion>
                   <IconCalendar />
-                  <C.Col>
-                    <C.H3>INÍCIO EM</C.H3>
-                    <C.H1>
-                      {courser.startTime}
-                    </C.H1>
+                </C.SvgAccordion>
+                <C.TextAccordion>3h</C.TextAccordion>
+              </C.DivAccordion>
+              {/* end 1 */}
 
-                  </C.Col>
+              {/* 2 */}
+              <C.RowAccordion>
+                <IconBook />
+                <C.H3>5 DE FEVEREIRO</C.H3>
+                <C.H3>18:30 - 22:30</C.H3>
+              </C.RowAccordion>
 
-                  <IconCalendar />
-                  <C.Col>
-                    <C.H3>TÉRMINO</C.H3>
-                    <C.H1>
-                      {courser.endDate}
-                    </C.H1>
-                  </C.Col>
-                </C.Row>
-
-                <C.Row>
-                  <IconBook />
-                  <C.Col>
-                    <C.H3>3 AULAS</C.H3>
-                  </C.Col>
-
+              <C.DivAccordion>
+                <C.TextAccordion>Parte 2</C.TextAccordion>
+                <C.SvgAccordion>
                   <IconHour />
-                  <C.Col>
-                    <C.H3>12H TOTAL</C.H3>
-                  </C.Col>
-                </C.Row>
+                </C.SvgAccordion>
+                <C.TextAccordion>3h</C.TextAccordion>
+              </C.DivAccordion>
+              {/* end 2 */}
 
-                <C.Row>
-                  <IconUser />
-                  <C.Col>
-                    <C.H3>
-                      {courser.speakers}
+              {/* 3 */}
+              <C.RowAccordion>
+                <IconBook />
+                <C.H3>6 DE FEVEREIRO</C.H3>
+                <C.H3>18:30 - 22:30</C.H3>
+              </C.RowAccordion>
 
-                    </C.H3>
-                  </C.Col>
+              <C.DivAccordion>
+                <C.TextAccordion>Parte 3</C.TextAccordion>
+                <C.SvgAccordion>
+                  <IconCalendar />
+                </C.SvgAccordion>
+                <C.TextAccordion>3h</C.TextAccordion>
+              </C.DivAccordion>
+              {/* end 3 */}
 
-                  <IconLocation />
-                  <C.Col>
-                    <C.H3>
-                      {courser.campus}
-                    </C.H3>
-                  </C.Col>
-                </C.Row>
-              </C.Information>
-
-              <C.DateTag>
-                <C.TextTag>
-                  {courser.enrollmentsStart}
-                </C.TextTag>
-
-              </C.DateTag>
-
-              <C.Button>
-                Página do Curso
-              </C.Button>
-            </C.Color>
-          </C.Container>
+            </C.InternalWrapper>
+          </C.AccordionWrapper>
         ))}
       </C.CardContainer>
+
     </div>
   );
 }
--- src/components/Card/styles.tsx
+++ src/components/Card/styles.tsx
@@ -84,7 +84,7 @@ export const Button = styled.button`
     position: absolute;
     width: 248px;
     height: 52px;
-    border: 0;
+    border: 0.1px;
     top: 365px;
     left: 10px;
     border-radius:15px;
@@ -99,4 +99,71 @@ export const CardContainer = styled.div`
     display: grid;
     grid-template-columns: repeat(4, 1fr);
     grid-gap: 15px;
+`;
+
+// accordion
+export const AccordionWrapper = styled(Container)`
+    height: auto;
+    transition: all 0.6s ease-in-out;
+`;
+
+export const InternalWrapper = styled.div`
+    width: 100%;
+    max-height: ${(props) => (props.open ? '300px' : '0')};
+    transition: all 1s ease-in-out;
+    overflow: hidden;
+`;
+
+export const ButtonWrapper = styled.button`
+border: 0px;
+background-color: transparent;
+`;
+
+export const TextWrapper = styled.p`
+    color: ${({ theme }) => theme.colors.black};
+    font-size: 13px;
+    font-weight: 600;
+    text-align: start;
+    width: 120px;
+`;
+
+export const RowWrapper = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    align-items: center;
+    height: 40px;
+`;
+export const RowAccordion = styled.div`
+    //background-color: #1221;
+    justify-content: space-around;
+    display: flex;
+    flex-direction: row;
+    width: 236px;
+    margin-top: 20px;
+    margin-left: 20px;
+    margin-bottom: 10px;
+`;
+export const ColWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content:center;
+`;
+export const DivAccordion = styled(RowAccordion)`
+    background-color: #FAFAFA;
+    align-items: center;
+    justify-content: space-between;
+    padding: 15px;
+    border-radius: 10px;
+    margin-top: 0px;
+    height: 40px;
+`;
+export const TextAccordion = styled.p`
+    color: ${({ theme }) => theme.colors.black};
+    font-size: ${({ theme }) => theme.fonts.h1};
+    font-weight: 600;
+`;
+export const SvgAccordion = styled.div`
+    display: flex;
+    margin-left: 100px;
 `;
