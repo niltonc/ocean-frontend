377d4c4ec731959829fae2cdbe9f292942b1d349
niltonc <niltoncesarjr1@gmail.com>
2022-04-24 02:47:22 -0300

    refactor(router): add a function

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -4,16 +4,15 @@ import CousersPage from '../pages/Cousers';
 import SchedulePage from '../pages/Schedule';
 import TreadsPage from '../pages/Treads';
 
-export interface IApplicationProps {}
-
-const Application: React.FunctionComponent<IApplicationProps> = () => (
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<CousersPage />} />
-      <Route path="/agenda" element={<SchedulePage />} />
-      <Route path="/trilhas" element={<TreadsPage />} />
-    </Routes>
-  </BrowserRouter>
-);
-
+function Application() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<CousersPage />} />
+        <Route path="/agenda" element={<SchedulePage />} />
+        <Route path="/trilhas" element={<TreadsPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
 export default Application;
