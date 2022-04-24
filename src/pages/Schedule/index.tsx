91cef0b093c64124735fa9c5d32eaa17ce5ae59c
niltonc <niltoncesarjr1@gmail.com>
2022-04-24 02:35:13 -0300

    refactor(schedule/treads): add function and react types

--- src/pages/Schedule/index.tsx
+++ src/pages/Schedule/index.tsx
@@ -1,18 +1,16 @@
 import React from 'react';
-import Footer from '../../components/footer';
 import Header from '../../components/header';
 import { Title } from '../../pages/Cousers/styles';
 
-export interface ISchedulePageProps {}
-
-const SchedulePage: React.FunctionComponent<ISchedulePageProps> = () => (
-  <div>
-    <Header />
-    <Title>
-      Agenda
-    </Title>
-    <Footer />
-  </div>
-);
+function SchedulePage() {
+  return (
+    <div>
+      <Header />
+      <Title>
+        Agenda
+      </Title>
+    </div>
+  );
+}
 
 export default SchedulePage;
--- src/pages/Treads/index.tsx
+++ src/pages/Treads/index.tsx
@@ -1,18 +1,15 @@
 import React from 'react';
-import Footer from '../../components/footer';
 import Header from '../../components/header';
 import { Title } from '../../pages/Cousers/styles';
 
-export interface ITreadsPageProps {}
-
-const TreadsPage: React.FunctionComponent<ITreadsPageProps> = () => (
-  <div>
-    <Header />
-    <Title>
-      Trilhas
-    </Title>
-    <Footer />
-  </div>
-);
-
+function TreadsPage() {
+  return (
+    <div>
+      <Header />
+      <Title>
+        Trilhas
+      </Title>
+    </div>
+  );
+}
 export default TreadsPage;
