f3a54eb2a1a4e5d17d1599a20a7fb210c4e0cc49
niltonc <niltoncesarjr1@gmail.com>
2022-04-24 03:07:39 -0300

    feat(setdata): add functions component

---
+++ src/components/Card/functions/setDate.tsx
@@ -0,0 +1,38 @@
+const setDate = (ISO: Date, Type: String) => {
+  const isoConverted = new Date(ISO);
+
+  const months = [
+    'JANEIRO',
+    'FEVEREIRO',
+    'MARÇO',
+    'ABRIL',
+    'MAIO',
+    'JUNHO',
+    'JULHO',
+    'AGOSTO',
+    'SETEMBRO',
+    'OUTUBRO',
+    'NOVEMBRO',
+    'DEZEMBRO',
+  ];
+  const month = isoConverted.getMonth();
+  const monthName = months[isoConverted.getMonth()];
+  const date = isoConverted.getDate();
+  const hour = isoConverted.getHours();
+  const min = isoConverted.getMinutes();
+
+  if (Type === 'initialCard') {
+    return `0${date}/0${month} ás ${hour}:${min}`;
+  }
+  if (Type === 'finalCard') {
+    return `${date}/${month}`;
+  }
+  if (Type === 'initialAccordion') {
+    return `${date} DE ${monthName}`;
+  }
+  if (Type === 'finalAccordion') {
+    return `${hour}:${min}`;
+  }
+};
+
+export default setDate;
