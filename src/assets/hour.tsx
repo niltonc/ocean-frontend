5ce2b2663e7005131362cb6358081feb74a9e5a4
niltonc <niltoncesarjr1@gmail.com>
2022-04-24 14:37:03 -0300

    feat(assets): add new png

--- src/assets/hour.tsx
+++ src/assets/hour.tsx
@@ -12,13 +12,13 @@ const Hour = (props: SVGProps<SVGSVGElement>) => (
       fillRule="evenodd"
       clipRule="evenodd"
       d="M6.949 1.25a5.624 5.624 0 1 0 0 11.25 5.624 5.624 0 1 0 0-11.25ZM.074 6.875a6.875 6.875 0 1 1 13.75 0 6.875 6.875 0 0 1-13.75 0Z"
-      fill="#00B9FF"
+      fill="currentColor"
     />
     <path
       fillRule="evenodd"
       clipRule="evenodd"
       d="M6.949 2.5c.345 0 .625.28.625.625v3.364l2.155 1.077a.626.626 0 0 1-.559 1.118l-2.5-1.25a.625.625 0 0 1-.346-.559v-3.75c0-.345.28-.625.625-.625Z"
-      fill="#00B9FF"
+      fill="currentColor"
     />
   </svg>
 );
