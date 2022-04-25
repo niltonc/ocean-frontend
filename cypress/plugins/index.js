9b8b35acbd7a0d4616618263fec6833ee13c292f
niltonc <niltoncesarjr1@gmail.com>
2022-04-25 07:48:27 -0300

    feat(cypress): add cypress

---
+++ cypress/plugins/index.js
@@ -0,0 +1,15 @@
+// cypress/plugins/index.js
+
+const path = require('path')
+const { startDevServer } = require('@cypress/vite-dev-server')
+
+module.exports = (on, config) => {
+  on('dev-server:start', (options) => {
+    return startDevServer({
+      options,
+      viteConfig: {
+        configFile: path.resolve(__dirname, '..', '..', 'vite.config.js'),
+      },
+    })
+  })
+}
